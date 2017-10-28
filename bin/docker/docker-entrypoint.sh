415f7b034d9d9915440afcb3d4d33c62ed9ea924
Sean Brandt <sean@fuzzymagic.com>
2017-10-28 17:36:54 -0400

    alternate settings approach

--- bin/docker/docker-entrypoint.sh
+++ bin/docker/docker-entrypoint.sh
@@ -14,6 +14,14 @@ echo ""
 echo "Filesystem Info"
 ls -latr /
 ls -latrR /run
+ls -latr /usr/src/game
+ls -latr /usr/src/game/conf
+
+if [ -f /run/secrets/evennia/secret_settings.py ]; then
+  ln -sf /run/secrets/evennia/secret_settings.py /usr/src/game/conf/secret_settings.py
+  echo "Linked secret settings into place."
+  ls -latr /usr/src/game/conf
+fi
 
 if [ "$1" = 'evennia' ]; then
   chown -R ${USER}:${USER} /usr/src/{evennia,game}
